94fd13a8cc3f9736da1ddea069604f26fa48b670
Vineeth S <svineth.face@gmail.com>
2017-03-06 23:03:44 +0530

    updated ui

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,169 +1,182 @@
 'use strict';
-var x;
-var initAttend=function (x) {
-  var ctx = document.getElementById("myChart").getContext('2d');
-  var courses=[];
-  var eth=[];
-  var ela=[];
-  for(var i=0;i<x.length;i++)
-  {
-    courses[i]=x[i].code;
-    eth[i]=x[i].theory.percentage;
-    ela[i]=x[i].lab.percentage;
-  }
-  var gdata= function(evt){
-    var activePoints = myChart.getElementAtEvent(evt);
-    console.log(activePoints);
-    // use _datasetIndex and _index from each element of the activePoints array
-  };
-  var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: courses,
-      datasets: [{
-        label: 'Theory',
-        data:eth ,
-        backgroundColor: "#4db6ac"
-      }, {
-        label: 'Lab',
-        data: ela,
-        backgroundColor: "#ffb74d"
-      }]
-    },
-    options: {
-      tooltip:{
-          callback:{
+var myChart;
+var port = chrome.runtime.connect({name: "MyVIT"});
+port.onMessage.addListener(function(msg) {
+    var processFormStatus=function(d,type){
+        return $('<p style="line-height: 15px;font-size: 15px;" class="center-align"><i style="line-height: 15px;font-size:18px;" class="material-icons">'+type+'</i>'+d+'</p>');
+    };
+    if (msg.isData)
+    {
+        $('.login_wrapper').addClass('hide');
+        $('.loading_wrapper').addClass('hide');
+        // $('#header').addClass('hide');
+        $('#heading').text("My VIT");
+        $('#graph').removeClass('hide');
+        $('#status').text("Logging In");
+        initAttend(msg.data);
+    }
+    else if (msg.isData==false)
+    {
+        console.log(msg.reason);
+        $('#graph').addClass('hide');
+        $('.loading_wrapper').addClass('hide');
+        if(msg.reason!='logout'&&msg.reason!=undefined)
+            Materialize.toast(processFormStatus(msg.reason,msg.type), 2000,'center-align');
+        $('.login_wrapper').removeClass('hide');
+        $('#heading').text("My VIT - Login");
+        $('#status').text("Logging In");
+    }
+    else if (msg.type=='Status-update'){
+        $('#status').text(msg.status);
+    }
 
-          }
-        },
-      scales: {
-        yAxes: [{
-          ticks: {
-            min: 0,
-            beginAtZero: true
-          }
-        }]
-      },
-      onClick:gdata,
-      hover: {
-        onHover: function(e) {
-          //console.log(e);
-          $("#myChart").css("cursor", e[0] ? "pointer" : "default");
-        }
-      }
-    }
-  });
-};
-var course=function (c) {
-  return {
-    code:c,
-    theory:{
-      total:null,
-      attended:null,
-      percentage:null
-    },
-    lab:{
-      total:null,
-      attended:null,
-      percentage:null
-    }
-  }
-};
-var ispresent=function(data,ele){
-  var f=-1;
-  for(var i=0;i<data.length;i++)
-  {
-    if(data[i].code==ele)
-    {
-      f=0;
-      return i;
-    }
-    else f=-1;
-  }
-  if(f==-1)
-  {
-    return f;
-  }
-};
-var islab=function (course) {
-  if(course.subject_type=='Embedded Lab'||course.subject_type=='Lab Only')
-    return 1;
-  else return 0;
-};
-var update=function(data,details,mode,i)
-{
-  var obj;
-  if(mode)
-    obj=data[i].lab;
-  else
-    obj=data[i].theory;
-  obj.total=details[0];
-  obj.attended=details[1];
-  obj.percentage=details[2];
-};
-var parse=function (x) {
-  var courses=x.courses;
-  var data=[],temp,isp,details=[],index;
-  console.log(courses);
-  for(var i=0;i<courses.length;i++)
-  {
-    details[0]=courses[i].attendance.total_classes;
-    details[1]=courses[i].attendance.attended_classes;
-    details[2]=courses[i].attendance.attendance_percentage;
-    isp=ispresent(data,courses[i].course_code);
-    if(isp==-1)
-    {
-      temp=new course(courses[i].course_code);
-      data.push(temp);
-      index=(data.length-1);
-    }
-    else {
-      index=isp;
-    }
-    update(data,details,islab(courses[i]),index);
-  }
-  console.log(data);
-  handleui(data);
-};
-var rqst=function (ch) {
-  var type=['login','refresh'];
-  var reg=document.getElementById('regno').value;
-  var pass=document.getElementById('pass').value;
-  $.ajax({
-    url:'https://myffcs.in:10443/campus/vellore/'+type[ch],
-    type: 'POST',
-    processData:false,
-    data:'regNo='+reg+'&psswd='+pass,
-    success:function (result) {
-      console.log(result);
-      if(ch==0)
-      {
-        $('#status').text('Retrieving Data ...');
-        rqst(1);
-      }
-      else
-        parse(result);
-    },
-    error: function(XMLHttpRequest, textStatus, errorThrown){
-      console.log("err--"+type[ch]+ XMLHttpRequest.status + " -- " + XMLHttpRequest.statusText);
-    }
-  });
-};
-var handleui =function(data)
-{
-  $('.loading_wrapper').addClass('hide');
-  $('#header').removeClass('hide');
-  $('#graph').removeClass('hide');
-  $('h1').text("My VIT");
-  initAttend(data);
-};
-var handle=function () {
-  $('.login_wrapper').addClass('hide');
-  $('#header').addClass('hide');
-  $('.loading_wrapper').removeClass('hide');
-  rqst(0);
-};
-$('#lbutton').click(function () {
-  handle();
 });
-// console.log('\'Allo \'Allo! Popup');
+var initAttend=function (x) {
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var courses=[],manipI,manipDI;
+    var ethbg=[],elabg=[];
+    var eth=[],eth_orig=[];
+    var ela=[],ela_orig=[];
+    for(var i=0;i<x.length;i++)
+    {
+        courses[i]=x[i].code;
+        eth[i]=x[i].theory.percentage;
+        ela[i]=x[i].lab.percentage;
+    }
+    courses[courses.length]='debarred';
+    eth[eth.length]=80;
+    ela[ela.length]=60;
+    var clickHandle= function(evt){
+        var activePoints = myChart.getElementAtEvent(evt);
+        var orig;
+        console.log(activePoints[0]);
+        manipI=activePoints[0]._index;
+        manipDI=activePoints[0]._datasetIndex;
+        $("#manipHead").text(courses[manipI]);
+        $("#manip").removeClass('disabledDiv');
+        if(manipDI)
+        {
+            elabg[manipI]='#ab47bc';
+        }
+        else
+        {
+            ethbg[manipI]='#ab47bc';
+        }
+        myChart.update();
+    };
+    var manipulate=function(){
+        var total,attended;
+        eth_orig=eth;
+        ela_orig=ela;
+        var spiners=[],manipVal;
+        spiners.push(document.getElementById('miss').value);
+        spiners.push(document.getElementById('attend').value);
+        if(manipDI)
+        {
+            attended=x[manipI].lab.attended;
+            total=x[manipI].lab.total;
+            manipVal=(((attended+spiners[1])/(total+spiners[0]+spiners[1]))*100);
+            console.log(manipVal,total,attended);
+            manipVal=Math.floor(manipVal);
+            // console.log(manipVal);
+            ela[manipI]=manipVal;
+        }
+        else
+        {
+            attended=x[manipI].theory.attended;
+            total=x[manipI].theory.total;
+            console.log('attended :',attended);
+            console.log('total :',total);
+            console.log('miss spinner:',spiners[0]);
+            console.log('attended spinner:',spiners[1]);
+            manipVal=(((+attended+ +spiners[1])/(+total+ +spiners[0]+ +spiners[1]))*100);
+            console.log(+manipVal,+attended+ +spiners[1],+total+ +spiners[0]+ +spiners[1]);
+            manipVal=Math.floor(manipVal);
+            // console.log(manipVal);
+            eth[manipI]=manipVal;
+        }
+        myChart.update();
+    };
+    $('#miss').on('input', function() {
+        if( +($(this).val())<0)
+            $(this).val(0);
+        manipulate();
+    });
+    $('#attend').on('input', function() {
+        if( +($(this).val())<0)
+            $(this).val(0);
+        manipulate();
+    });
+    for(var k=0;k<eth.length;k++)
+    {
+        if(eth[k]<75)
+            ethbg.push('#ef5350');
+        else
+            ethbg.push('#4db6ac');
+        if(ela[k]<75)
+            elabg.push('#ef5350');
+        else
+            elabg.push('#ffb74d');
+    }
+    myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: courses,
+            datasets: [{
+                label: 'Theory',
+                data:eth ,
+                backgroundColor: ethbg
+            }, {
+                label: 'Lab',
+                data: ela,
+                backgroundColor: elabg
+            }]
+        },
+        options: {
+            tooltip:{
+                callback:{
+
+                }
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        min: 0,
+                        beginAtZero: true
+                    }
+                }]
+            },
+            onClick:clickHandle,
+            hover: {
+                onHover: function(e) {
+                    $("#myChart").css("cursor", e[0] ? "pointer" : "default");
+                }
+            }
+        }
+    });
+};
+
+$('#lbutton').click(function () {
+    var reg=document.getElementById('regno').value;
+    var pass=document.getElementById('pass').value;
+    port.postMessage({req:'Set-Reg-Pass',Reg:reg,Pwd:pass});
+    $('#heading').text("");
+    $('.login_wrapper').addClass('hide');
+    $('.loading_wrapper').removeClass('hide');
+});
+$('#refresh').click(function () {
+    $('#graph').addClass('hide');
+    $('#heading').text("");
+    $('#status').text('Refreshing..');
+    $('.loading_wrapper').removeClass('hide');
+    myChart.destroy();
+    port.postMessage({req:'refresh'});
+});
+$('#logout').click(function () {
+    $('#graph').addClass('hide');
+    $('#heading').text("");
+    $('#status').text('Logging out..');
+    $('.loading_wrapper').removeClass('hide');
+    port.postMessage({req:'logout'});
+    myChart.destroy();
+});
