ae18e76d33c4de9ca6d08b372b8bb8de40b9b8c8
Vineeth S <svineth.face@gmail.com>
2017-04-09 01:45:20 +0530

    Injection initialised

--- app/scripts/coursePage.js
+++ app/scripts/coursePage.js
@@ -1,5 +1,7 @@
 $(function () {
-   alert("Course page detected !!");
+   $('body').append(`<div style="margin-top:-200px;max-width: 800px;">
+<iframe style="width:100%;" src="${chrome.extension.getURL ('course_page/index.html')}" frameborder="0"></iframe>
+</div>`);
 });
 
 // let port = chrome.runtime.connect({name: "MyVIT_coursePage"});
